7af6449e575d3631c2048e0648e4241b21d70662
Arnar Freyr Kristinsson <arnarfreyr@hive.is>
2015-11-04 11:10:25 +0000

    Login og sign-up
    Núna er hægt að logga sig inn og nýskrá sig

--- login.php
+++ login.php
@@ -1,9 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>dub16</title>
+  <meta charset="utf-8">
+</head>
+<body>
 <?php
 include_once "dbconnection.php";
 include_once "update_database.php";
-
-if (isset($_POST['subnyskra'])) {//Ef ýtt er á Nýskrá þá er allt kennitala, nafn, lykilorð, tölvupóstur og sími sett í post arrayið.
-
 
 $kt = $_POST['kt'];
 $nafn = $_POST['user'];
@@ -11,23 +15,54 @@ $pass = $_POST['password'];
 $email = $_POST['email'];
 $simi = $_POST['simi'];
 //Hashar lykilorðið með sha1 algorithmanum 
-
 $pass = sha1($pass);
-$testing = sha1("123");//Til að prófa hvort hægt sé að bera saman hash gildin.
 
-echo $pass." ";
-echo $testing;
-
-if ($pass == $testing) {
-	echo "Yay!!!!";
-}
-
-
-}
-
+//FYRIR LOGIN
 if (isset($_POST['sublogin'])) {
-	
+
+   $login_kt = $_POST['login_kt'];
+   $login_pass = $_POST['login_password'];
+
+	//Bara að checka hvort að ég get staðfest að lykilorðið sé rétt
+   if (strlen($login_pass)) {
+   	
+   
+if ($notandi_pass[0] == sha1($login_pass) && $notandi_pass[0] != null) {
+	//Hér kemur allt sen kemur eftir að notandi loggar sig inn.
+  echo "Halló, ". $login_kt;
+
+echo '<form class="pure-form-stacked" action="login.php" method="POST">
+    <h1>Mínir Viðburðir</h1>';
+    //Pælingin hér er að láta lítið box birtast fyrir hvern atburð
+
+    foreach ($nafn_atburdar as $heiti_atburdar) {
+    	echo '<div class="events_box">';
+         echo '<div class="heiti_atburdar">'.$heiti_atburdar."</div>";
+
+    	echo '</div>';
+    }
+
+     
+   
+   echo '</div>
+</form>';
+
 }
 
+else{
+	if (!isset($notandi_pass)) {
+		
+	echo "Rangt notandanafn eða lykilorð";
+}
+else{
+	echo "Rangt notandanafn eða lykilorð";
+	}
+}
+}
+
+}
 
 ?>
+
+</body>
+</html>
